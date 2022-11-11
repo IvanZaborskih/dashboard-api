5b776c412ab5e2d97aed99591aa8642b0003d25d
IvanZaborskih <ivan.31.03@mail.ru>
2022-11-11 18:53:13 +0300

    User controller

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,31 @@
 import express, { Express } from "express";
-import { userRouter } from "./users/users";
 import { Server } from 'http';
 import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
 
 export class App {
 	app: Express;
 	port: number;
 	server: Server;
 	logger: LoggerService;
+	userController: UserController;
 
-	constructor(logger: LoggerService) {
+	constructor(
+		logger: LoggerService,
+		userController: UserController
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
+		this.userController = userController;
 	}
 
-	userRouter() {
-		this.app.use('/users', userRouter);
+	useRoutes() {
+		this.app.use('/users', this.userController.router);
 	}
 
 	public async init() {
-		this.userRouter();
+		this.useRoutes();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 	}
